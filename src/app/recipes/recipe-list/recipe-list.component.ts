426b359d09f310339bc22425a02e937129f98b9e
kaiching0109 <aaronsuen0109@gmail.com>
2020-07-10 09:49:30 +0900

    UPDATE: show recipe conditionally (Passing Data with Event and Property Binding)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
+  @Output() recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://s3-ap-northeast-1.amazonaws.com/snpd-tokyo-user-profile-icon/52f42d75fdf45f6bd6e3eb1c.jpg'),
     new Recipe('A Test Recipe', 'This is simply a test', 'https://s3-ap-northeast-1.amazonaws.com/snpd-tokyo-user-profile-icon/52f42d75fdf45f6bd6e3eb1c.jpg'),
   ];
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipeEl: Recipe){
+    this.recipeWasSelected.emit(recipeEl)
   }
 
 }

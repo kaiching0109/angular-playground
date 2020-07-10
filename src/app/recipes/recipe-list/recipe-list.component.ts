9ffb3ca0ed0abde28810c58fba1746e59fdd912e
kaiching0109 <aaronsuen0109@gmail.com>
2020-07-10 09:56:16 +0900

    UPDATE: show recipe details (Passing Data with Event and Property Binding)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://s3-ap-northeast-1.amazonaws.com/snpd-tokyo-user-profile-icon/52f42d75fdf45f6bd6e3eb1c.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://s3-ap-northeast-1.amazonaws.com/snpd-tokyo-user-profile-icon/52f42d75fdf45f6bd6e3eb1c.jpg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://s3-ap-northeast-1.amazonaws.com/snpd-tokyo-user-profile-icon/52f42d75fdf45f6bd6e3eb1c.jpg'),
   ];
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();

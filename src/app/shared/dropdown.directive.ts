0fe25cecd5342709df83232a096dac497f06560e
kaiching0109 <aaronsuen0109@gmail.com>
2020-07-14 09:40:46 +0900

    UPDATE: custom directive with ajs

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core'
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropdownDirective {
+    //listen to click
+    //toggle class open to btn-group
+    /**
+     *
+     */
+    isOpen = false;
+    @HostBinding('class.open')  public bindStyle: boolean = false;
+
+    constructor(private elRef: ElementRef, private renderer: Renderer2) {
+
+    }
+
+    ngOnInit() {
+    }
+
+    @HostListener('click') toggleOpen(eventData: Event) {
+        console.log('hi')
+        // if(!this.isOpen)
+        //     this.renderer.addClass(this.elRef, 'open');
+        // else 
+        //     this.renderer.removeClass(this.elRef, 'open');
+        this.bindStyle = !this.bindStyle;
+    }       
+}

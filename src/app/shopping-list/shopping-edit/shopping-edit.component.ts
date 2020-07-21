593e0b1a45b94598719359626fc9b1c06a5dc8f2
kaiching0109 <aaronsuen0109@gmail.com>
2020-07-21 16:23:32 +0900

    UPDATE: add ADD, DELETE, DELETE ALL, and CLEAR functionalies

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from 'src/services/shopping-list.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,18 +10,57 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit{
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  @ViewChild('f') slForm: NgForm;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit(): void {
+    this.slService.startEditting.subscribe((index: number) => {
+      console.log({ index })
+      this.editMode = true;
+      this.editedItemIndex = index;
+      const ingredientToEdit = this.slService.getIngredient(this.editedItemIndex);
+      if(ingredientToEdit) {
+        const { name, amount } = ingredientToEdit;
+        this.slForm.setValue({ name, amount })
+      }
+    })
   }
 
-  onAddItem(form: NgForm) {
-    if(form) {
-      const { value: { name, amount } } = form;
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  onSubmitItem() {
+    if(this.slForm) {
+      const { value: { name, amount } } = this.slForm;
       const newIngredient = new Ingredient(name, amount);
-      this.slService.onIngredientAdded(newIngredient);
+      if(this.editMode) {
+        this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+      }
+      else this.slService.onIngredientAdded(newIngredient);
     }
+    this.editMode = false;
+    this.slForm.reset();
+  }
+
+  onRemoveItem() {
+    if(this.slForm && this.editedItemIndex > -1) {
+      this.slService.removeIngredient(this.editedItemIndex);
+      this.slForm.reset();
+    }
+  }
+
+  onRemoveAllItems(){
+    this.slForm.reset();
+    this.slService.onIngredientReset();
+  }
+
+  onResetItem() {
+    this.slForm.reset();
   }
 
 }
--- src/services/shopping-list.service.ts
+++ src/services/shopping-list.service.ts
@@ -1,25 +1,44 @@
 import { Ingredient } from "../app/shared/ingredient.model";
-import { EventEmitter } from '@angular/core';
+import { Subject } from "rxjs";
 
 export class ShoppingListService {
     private ingredients: Ingredient[] = [
         new Ingredient("Apples", 5),
         new Ingredient("Tomatoes", 10)
     ];
-    ingredientsChanged =  new EventEmitter<Ingredient[]>();
+    ingredientsChanged =  new Subject<Ingredient[]>();
+    startEditting = new Subject<number>();   
     
     getIngredients() {
-        return [...this.ingredients]
+        return [...this.ingredients];
+    }
+
+    getIngredient(index: number) {
+        return this.ingredients[index];
     }
 
     onIngredientAdded(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit([...this.ingredients]);
+        this.ingredientsChanged.next([...this.ingredients]);
     }
 
     onIngredientsAdded(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit([...this.ingredients]);
+        this.ingredientsChanged.next([...this.ingredients]);
     }
 
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next([...this.ingredients]);
+    }
+
+    onIngredientReset() {
+        this.ingredients = [];
+        this.ingredientsChanged.next([])
+    }
+
+    removeIngredient(index: number) {
+        this.ingredients = this.ingredients.filter((ingredient, i) => i !== index);
+        this.ingredientsChanged.next([...this.ingredients])
+    }
 }

60d19f7f77effd703d653aa3783c0e8147db086c
kaiching0109 <aaronsuen0109@gmail.com>
2020-07-28 15:58:07 +0900

    UPDATE: able to login with and fetch with token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { catchError, tap } from 'rxjs/operators';
-import { throwError, Subject } from 'rxjs';
+import { throwError, BehaviorSubject } from 'rxjs';
 import { User } from "./user.model";
 
 const API_KEY = "AIzaSyB8cL5aoZxOmGevRqVtAUbvtIQwSDBr8_o";
@@ -20,7 +20,7 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-    user = new Subject<User>();
+    user = new BehaviorSubject<User>(null);
 
     constructor(private http: HttpClient) {}
 
@@ -36,7 +36,10 @@ export class AuthService {
     signin(email: string, password: string) {
         return this.http.post<AuthResponseData>(`${SIGN_IN_URL}${API_KEY}`, {
             email, password, returnSecureToken: true
-        }).pipe(catchError(this.handleError))
+        }).pipe(catchError(this.handleError), tap(resData => {
+            const { email, localId, idToken, expiresIn } = resData;
+            this.handleAuthentication(email, localId, idToken, +expiresIn);
+        }))
     }
 
     handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { RecipeService } from 'src/services/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({ providedIn: 'root'  })
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) {
+    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {
 
     }
 
     getRecipes() {
-        return this.http.get<Recipe[]>('https://ng-complete-guide-94c21.firebaseio.com/recipes.json')
-        .pipe(
-            map(recipes => {
-                console.log('here')
-                return recipes.map(recipe => ({...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}));
-            }),
-            tap(recipes => {
-                console.log(recipes)
-                this.recipeService.setRecipes(recipes)
+        console.log('123123123')
+        return this.authService.user.pipe(
+        take(1), 
+        exhaustMap(user => {
+            console.log({user})
+            return this.http.get<Recipe[]>('https://ng-complete-guide-94c21.firebaseio.com/recipes.json', {
+                params: new HttpParams().set('auth', user?.token)
             })
+        }),
+        map(recipes => {
+            console.log('here')
+            return recipes.map(recipe => ({...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}));
+        }),
+        tap(recipes => {
+            console.log(recipes)
+            this.recipeService.setRecipes(recipes)
+        })
         )
     }
 
